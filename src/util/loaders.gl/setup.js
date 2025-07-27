0ecaf36cc081d583053ca20163960dbe1bc101a7
Andreas Plesch <andreasplesch@users.noreply.github.com>
2025-07-27 20:37:48 +0000

    loadersgl

---
+++ src/util/loaders.gl/setup.js
@@ -0,0 +1,7 @@
+// sets up for use of the loaders.gl npm libraries
+// needs to be executed before libraries are added with packages.json
+// establish a summy exports object
+exports = {};
+// establish an x3dom object as name space
+x3dom.loaders = {};
+// the npm libraries are amended by a line to save the libraries into the name space
